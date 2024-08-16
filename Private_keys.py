e1612b54a8521675ee4ba9a90f46c0d166bbbe32
Syed Zain Ul Abideen <goodtingbye2@gmail.com>
2024-08-16 07:52:51 +0500

    fix safety issue

--- Private_keys.py
+++ Private_keys.py
@@ -1,4 +1,4 @@
 account_sid = "ACa01ad346b90457c0c107fefe8f0d91fb"
-auth_token = "b2f1c0daae185933e7571d7f7b0ae1e2"
+auth_token = "N/A"
 twillio_default_number = "+14155238886"
-user_phone_number = "+923056117017"
+user_phone_number = "N/A"

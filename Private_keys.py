32daf8837de938fd7385cd870764c411237d5606
Syedzain08 <goodtingbye2@gmail.com>
2024-08-22 18:29:43 +0500

    Update Private_keys.py

--- Private_keys.py
+++ Private_keys.py
@@ -1,9 +1,4 @@
-muzna_phone = "+923001834636"
-zain_phone = "+923056117017"
-
-
-
 account_sid = "ACa01ad346b90457c0c107fefe8f0d91fb"
-auth_token = "201551445ac36428652b167202d52d64"
-twillio_default_number = "+14155238886"
-user_phone_number = muzna_phone
+auth_token = "N/A"
+twillio_default_number = "N/A"
+user_phone_number = "N/A"
